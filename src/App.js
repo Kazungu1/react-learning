4f86fbaf63b671c751b8a39c4bfff638c0004252
Kazungu1 <kazunguderriq@gmail.com>
2022-07-14 11:26:22 +0300

    add and style components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem />
+      < ExpenseItem />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
 
-    return <h2>Expense Item!</h2>
+
+function ExpenseItem() {
+    const expenseDate = new Date(2022, 7,14);
+    const expenseItem= "School-Bag";
+    const expenseAmount = 3500;
+
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseItem}</h2>
+        <div className='expense-item__price'> {expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
